01701f38ed9197be6a7a92f5640f103ec2582752
Ahmet Kök <kokahmet@pm.me>
2024-06-17 12:17:22 +0300

    analytics and speed insights added

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,7 +5,7 @@ import { Inter as FontSans } from "next/font/google";
 import "@/app/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from "@vercel/analytics/react";
+
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -73,7 +73,6 @@ export default async function LocaleLayout({
             {children}
           </ThemeProvider>
         </NextIntlClientProvider>
-        <Analytics />
       </body>
     </html>
   );
